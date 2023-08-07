84f8adaf5f5ec42a832977ec81e04995486b9f03
João Pedro Almeida <33964846+Touratica@users.noreply.github.com>
2023-08-07 20:23:36 +0100

    feat: send internal email on order placement (#2)
    * chore(deps): install @sendgrid/mail
    
    * fix(db): fix typos in column names
    
    * feat: send email on order placement

--- src/app/api/orders/route.ts
+++ src/app/api/orders/route.ts
@@ -1,6 +1,7 @@
 import { prisma } from "@/lib/prisma";
 import { OrderValidator } from "@/lib/validators/order";
-import { Buyer } from "@prisma/client";
+import type { Buyer, Order, OrderItem, Product } from "@prisma/client";
+import sgMail from "@sendgrid/mail";
 import { NextResponse } from "next/server";
 import { z } from "zod";
 
@@ -50,6 +51,41 @@ async function getOrUpsertBuyer({
   return buyer;
 }
 
+async function sendMail(
+  order: Order & { orderItems: (OrderItem & { product: Product })[] },
+  buyer: Buyer
+) {
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY!);
+
+  const msg: sgMail.MailDataRequired = {
+    from: process.env.SENDGRID_FROM_EMAIL!,
+    to: process.env.SENDGRID_TO_EMAIL!,
+    subject: `Encomenda #${order.id}`,
+    html: `<h1>Encomenda #${order.id}</h1>
+    <p>Nome: ${buyer.firstName} ${buyer.lastName}</p>
+    <p>NIF: ${buyer.vatId}</p>
+    <p>Email: ${buyer.email}</p>
+    <p>Telemóvel: ${buyer.phone}</p>
+    <p>Produto: ${order.orderItems[0].product.name}</p>
+    <p>Tamanho: ${order.orderItems[0].size}</p>
+    <p>Nome personalizado: ${order.orderItems[0].personalizedName}</p>
+    <p>Número personalizado: ${order.orderItems[0].personalizedNumber}</p>
+    <p>Quantidade: ${order.orderItems[0].quantity}</p>`,
+  };
+
+  try {
+    await sgMail.send(msg);
+  } catch (error: any) {
+    console.error(error);
+
+    if (error?.response) {
+      console.error(error.response.body);
+    }
+
+    throw new Error("Could not send email", { cause: "SENDGRID_ERROR" });
+  }
+}
+
 export async function POST(req: Request) {
   try {
     const body = await req.json();
@@ -77,7 +113,7 @@ export async function POST(req: Request) {
     const order = await prisma.order.create({
       data: {
         buyerId: buyer.id,
-        OrderItem: {
+        orderItems: {
           create: {
             productId,
             size: productSize,
@@ -87,13 +123,20 @@ export async function POST(req: Request) {
           },
         },
       },
+      include: {
+        orderItems: { include: { product: true } },
+      },
     });
+
+    await sendMail(order, buyer);
 
     return new NextResponse(JSON.stringify(order), { status: 201 });
   } catch (error) {
     if (error instanceof z.ZodError) {
       return new NextResponse(error.message, { status: 400 });
     }
+
+    console.error(error);
 
     return new NextResponse(
       "Could not place order at this time. Please try later",
