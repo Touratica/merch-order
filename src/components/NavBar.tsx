16ff367e9d0858e9fb88ada5a7e53965a9fb074b
João Pedro Almeida <33964846+Touratica@users.noreply.github.com>
2023-09-11 17:29:44 +0100

    fix(ui): Adjust link to cover just logo and name

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,18 +4,20 @@ import type { ReactElement } from "react";
 
 export default function NavBar(): ReactElement {
   return (
-    <Link
-      href="https://korfballx.pt"
-      className="mb-3 inline-flex w-full items-center bg-green-800 p-3 text-2xl font-semibold text-white "
-    >
-      <Image
-        src="/klx.svg"
-        alt="Logo"
-        width={75}
-        height={75}
-        className="mr-2"
-      />
-      <h1>Clube Korfball de Lisboa</h1>
-    </Link>
+    <div className="mb-3 w-full bg-green-800 text-2xl font-semibold text-white">
+      <Link
+        href="https://korfballx.pt"
+        className="inline-flex items-center p-3"
+      >
+        <Image
+          src="/klx.svg"
+          alt="Logo"
+          width={75}
+          height={75}
+          className="mr-2"
+        />
+        <h1>Clube Korfball de Lisboa</h1>
+      </Link>
+    </div>
   );
 }
